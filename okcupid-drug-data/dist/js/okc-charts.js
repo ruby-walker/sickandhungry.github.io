d97abf57bbb7e661abdf6dc06fc17b21c57bf38e
Ruby Walker <avw7@pitt.edu>
2017-02-09 15:53:52 -0500

    Add files via upload

--- okcupid-drug-data/dist/js/okc-charts.js
+++ okcupid-drug-data/dist/js/okc-charts.js
@@ -1,16 +1,16 @@
 google.charts.load("current", {packages:["corechart"]});
 google.charts.setOnLoadCallback(drawAgeNeverChart);
 google.charts.setOnLoadCallback(drawAgeSometimesChart);
-google.charts.setOnLoadCallback(drawAgeNeverChart);
+google.charts.setOnLoadCallback(drawAgeOftenChart);
 function drawAgeNeverChart() {
   var data = google.visualization.arrayToDataTable([
     ['Age Bracket', 'User'],
     ['18-21',     1860],
-    ['26-30',     6453],
+    ['22-25',     6453],
+    ['26-30',     10551],
     ['31-40',     11232],
     ['41-50',     4767],
-    ['51-60',     2071],
-    ['61-70',     789]
+    ['51+',     2860]
   ]);
 
   var options = {
@@ -32,12 +32,12 @@ chart.draw(data, options);
   function drawAgeSometimesChart() {
     var data = google.visualization.arrayToDataTable([
       ['Age Bracket', 'User'],
-      ['18-21',     2],
-      ['26-30',     3],
-      ['31-40',     6],
-      ['41-50',     2],
-      ['51-60',     5],
-      ['61-70',     2]
+      ['18-21',     767],
+      ['22-25',     1979],
+      ['26-30',     2375],
+      ['31-40',     1904],
+      ['41-50',     556],
+      ['51+',     250],
     ]);
 
     var options = {
@@ -60,13 +60,14 @@ chart.draw(data, options);
 function drawAgeOftenChart() {
   var data = google.visualization.arrayToDataTable([
     ['Age Bracket', 'User'],
-    ['18-21',     2],
-    ['26-30',     3],
-    ['31-40',     6],
-    ['41-50',     2],
-    ['51-60',     5],
-    ['61-70',     2]
+    ['18-21',     103],
+    ['22-25',     132],
+    ['26-30',     91],
+    ['31-40',     59],
+    ['41-50',     13],
+    ['51+',       5]
   ]);
+
 
   var options = {
     pieHole: 0.4,
